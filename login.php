b2daff560f2ec7a3eb44e4d4afdf0534065e3ae5
muneeb706 <muneeb706@gmail.com>
2020-08-23 01:55:33 -0500

    title updated.

--- login.php
+++ login.php
@@ -10,7 +10,7 @@ if(!empty($_SESSION["sessionId"]) or isset($_SESSION["sessionId"])) {
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Organization Name | Log In</title>
+    <title>Organization Name - DES | Log In</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
