4db892669d0c109b7b91ff3149b4538b15c9e6ff
Muneeb Shahid <muneeb706@gmail.com>
2024-02-01 20:46:26 -0800

    feat(login): Login Workflow added #1

--- core/views.py
+++ core/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from .forms import AuthenticationForm, LoginForm
+
 
 def login_view(request):
-    return render(request, 'login.html')
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = LoginForm()
+    return render(request, "login.html", {"form": form})
